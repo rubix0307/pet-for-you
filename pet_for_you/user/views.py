098d7fd05ea3eae4939de00b21929298adf807b9
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-11 13:56:22 +0200

    create test profile +use htmX

--- pet_for_you/user/views.py
+++ pet_for_you/user/views.py
@@ -1,3 +1,13 @@
+import time
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    return render(request, 'user/profile.html')
+
+def profile_main_user_data(request):
+    return render(request, 'user/inc/_profile_main_user_data.html')
+
+def profile_main_user_data_form(request):
+    return render(request, 'user/inc/_profile_main_user_data_form.html')
