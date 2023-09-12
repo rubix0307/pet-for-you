1151a2668d367b12504d7829c41be96a79e57116
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-12 09:08:51 +0200

    add contact page

--- pet_for_you/main/urls.py
+++ pet_for_you/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- pet_for_you/main/views.py
+++ pet_for_you/main/views.py
@@ -6,3 +6,9 @@ def main(request):
         title='Главная страница'
     )
     return render(request, 'main/index.html', context=context)
+
+def contacts(request):
+    context = dict(
+        title='Страница контактов'
+    )
+    return render(request, 'main/contacts.html', context=context)
