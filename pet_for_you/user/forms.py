7d79a588d3566f15b89b62d4d2d64a54a0ba3c7d
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-15 15:49:16 +0200

    add user auth forms

---
+++ pet_for_you/user/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+from .models import CustomUser
+
+
+class UserSignInForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        authenticated_user = authenticate(email=email, password=password)
+        if not authenticated_user:
+            raise forms.ValidationError('Пароли не совпадают.')
+        else:
+            cleaned_data.update(dict(password=authenticated_user.password))
+
+        return cleaned_data
+
+class UserRegistrationForm(forms.ModelForm):
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput,
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email', 'phone', 'password', 'password2']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError('Пароли не совпадают.')
+        else:
+            raise forms.ValidationError('Пароли не заполнены.')
+
+        return cleaned_data
