84c73baee3de98bd9893a578aaf72fc57da9bc54
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-18 21:37:03 +0200

    create blog_index logic

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.core.handlers.wsgi import WSGIRequest
+
+from blog.models import Blog, Tag
 
 
 def blog_index(request: WSGIRequest):
-    return render(request, 'blog/index.html')
+    context = dict()
+    tag_name = request.GET.get('tag')
+
+    posts = Blog.objects.order_by('-id')
+    try:
+        if tag_name:
+            tag = Tag.objects.get(name=tag_name)
+            posts = posts.filter(tag=tag)
+            context.update(dict(is_tag=True))
+    except Tag.DoesNotExist:
+        messages.error(request, 'Искомый тег не существует')
+
+    finally:
+        posts = posts.all()
+        tags = Tag.objects.filter(blog__isnull=False).distinct()
+        context.update(dict(
+            posts=posts,
+            tags=tags,
+        ))
+
+    return render(request, 'blog/index.html', context=context)
