d9f1fb1207f1e78ae0690ecb87049e2dcc33ae7c
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-18 21:36:36 +0200

    add extra data

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,25 @@ class Tag(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
 class Blog(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     text = models.TextField()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
 
 class Feedback(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,3 +32,10 @@ class Feedback(models.Model):
     media = models.CharField(max_length=255)
     user = models.ForeignKey('user.customuser', on_delete=models.CASCADE)
     animal = models.ForeignKey('animal.animal', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
