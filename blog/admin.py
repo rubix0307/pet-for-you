ec96f3c1deab17cf7fcdd7bf1f48cbea411ab408
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-18 21:36:12 +0200

    register in django-admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Tag, Blog, Feedback
 # Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+    ordering = ('id',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'tag')
+    list_display_links = ('title',)
+    ordering = ('-id',)
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'animal')
+    list_display_links = ('title',)
+    ordering = ('-id',)
+
