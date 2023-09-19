be425beed17963803b4ad698cd803b9d6978a8bc
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-19 17:10:38 +0200

    add animal page

--- animal/urls.py
+++ animal/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from . import views
 
 urlpatterns = [
+    path('', views.animal_index, name='animal_index'),
 ]
--- animal/views.py
+++ animal/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.core.handlers.wsgi import WSGIRequest
+from .models import Animal, Sex
 # Create your views here.
+
+def animal_index(request: WSGIRequest):
+    context = dict()
+
+    animals = Animal.objects.order_by('-id').all()
+
+    context.update(dict(
+        animals=animals,
+    ))
+
+    return render(request, 'animal/index.html', context=context)
