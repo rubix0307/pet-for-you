fc91077b130ca324e0eab7a69f013ebc9c8cf897
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-09-30 00:23:02 +0200

    update unittests

--- animal/tests.py
+++ animal/tests.py
@@ -1,3 +1,114 @@
+import random
+import unittest
+import datetime as dt
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from animal.utils import comb, get_available_pet_walk_time
+
+
+class TestWalkSchedule(unittest.TestCase):
+    walk_times = [
+        (comb(8, 0), comb(8, 15)),
+        (comb(9, 30), comb(10, 0)),
+        (comb(10, 30), comb(11, 0)),
+        (comb(11, 30), comb(12, 0)),
+        (comb(12, 30), comb(13, 0)),
+        (comb(13, 45), comb(14, 0)),
+        (comb(14, 30), comb(15, 0)),
+        (comb(16, 30), comb(17, 0)),
+        (comb(17, 15), comb(17, 30)),
+    ]
+    selected_day = datetime.combine(datetime.now(), dt.time(hour=8))
+
+    def test_duration_15(self):
+        test_available_pet_walk_time = get_available_pet_walk_time(
+            walk_times=self.walk_times,
+            duration=15,
+            selected_day=self.selected_day,
+        )
+        expected_result = [comb(8, 15),  comb(8, 30),
+                  comb(8, 45),  comb(9, 0),
+                  comb(9, 15),  comb(10, 0),
+                  comb(10, 15), comb(11, 0),
+                  comb(11, 15), comb(12, 0),
+                  comb(12, 15), comb(13, 0),
+                  comb(13, 15), comb(13, 30),
+                  comb(14, 0),  comb(14, 15),
+                  comb(15, 0),  comb(15, 15),
+                  comb(15, 30), comb(15, 45),
+                  comb(16, 0),  comb(16, 15),
+                  comb(17, 0),  comb(17, 30),
+                  comb(17, 45),]
+        self.assertEqual(test_available_pet_walk_time, expected_result)
+
+    def test_duration_30(self):
+        test_available_pet_walk_time = get_available_pet_walk_time(
+            walk_times=self.walk_times,
+            duration=30,
+            selected_day=self.selected_day,
+        )
+        expected_result = [comb(8, 15),  comb(8, 30),  comb(8, 45),  comb(9, 00),
+                  comb(10, 00), comb(11, 00), comb(12, 00), comb(13, 00),
+                  comb(13, 15), comb(14, 00), comb(15, 00), comb(15, 15),
+                  comb(15, 30), comb(15, 45), comb(16, 00), comb(17, 30),]
+        self.assertEqual(test_available_pet_walk_time, expected_result)
+
+    def test_duration_60(self):
+        test_available_pet_walk_time = get_available_pet_walk_time(
+            walk_times=self.walk_times,
+            duration=60,
+            selected_day=self.selected_day,
+        )
+        expected_result = [comb(8, 15), comb(8, 30), comb(15, 00), comb(15, 15), comb(15, 30), ]
+        self.assertEqual(test_available_pet_walk_time, expected_result)
+
+    def test_duration_60_mixed(self):
+        test_walk_times = self.walk_times[:]
+        random.shuffle(test_walk_times)
+
+        available_pet_walk_time = get_available_pet_walk_time(
+            walk_times=test_walk_times,
+            duration=60,
+            selected_day=self.selected_day
+        )
+        expected_result = [comb(8, 15), comb(8, 30), comb(15, 00), comb(15, 15), comb(15, 30), ]
+        self.assertEqual(available_pet_walk_time, expected_result)
+
+    def test_not_selected_day(self):
+        available_pet_walk_time = get_available_pet_walk_time(
+            walk_times=self.walk_times,
+            duration=60,
+        )
+        expected_result = [comb(8, 15), comb(8, 30), comb(15, 00), comb(15, 15), comb(15, 30), ]
+        self.assertEqual(available_pet_walk_time, expected_result)
+
+    def test_next_selected_day_and_empty_walk_times(self):
+        selected_day = datetime.now() + dt.timedelta(days=1)
+        next_selected_daty_walk = get_available_pet_walk_time(
+            walk_times=self.walk_times,
+            duration=60,
+            selected_day=selected_day,
+        )
+        empty_walk_times = get_available_pet_walk_time(
+            walk_times=[],
+            duration=60,
+            selected_day=selected_day,
+        )
+
+        expected_result = [comb(8, 0, selected_day), comb(8, 15, selected_day), comb(8, 30, selected_day), comb(8, 45, selected_day),
+                  comb(9, 0, selected_day), comb(9, 15, selected_day), comb(9, 30, selected_day), comb(9, 45, selected_day),
+                  comb(10, 0, selected_day), comb(10, 15, selected_day), comb(10, 30, selected_day), comb(10, 45, selected_day),
+                  comb(11, 0, selected_day), comb(11, 15, selected_day), comb(11, 30, selected_day), comb(11, 45, selected_day),
+                  comb(12, 0, selected_day), comb(12, 15, selected_day), comb(12, 30, selected_day), comb(12, 45, selected_day),
+                  comb(13, 0, selected_day), comb(13, 15, selected_day), comb(13, 30, selected_day), comb(13, 45, selected_day),
+                  comb(14, 0, selected_day), comb(14, 15, selected_day), comb(14, 30, selected_day), comb(14, 45, selected_day),
+                  comb(15, 0, selected_day), comb(15, 15, selected_day), comb(15, 30, selected_day), comb(15, 45, selected_day),
+                  comb(16, 0, selected_day), comb(16, 15, selected_day), comb(16, 30, selected_day), comb(16, 45, selected_day),
+                  comb(17, 0,selected_day),]
+
+        self.assertEqual(next_selected_daty_walk, expected_result)
+        self.assertEqual(empty_walk_times, expected_result)
+
+
--- animal/utils.py
+++ animal/utils.py
@@ -1,7 +1,9 @@
-import datetime
 import datetime as dt
-from typing import List, Tuple, Union
-import unittest
+from datetime import datetime, timedelta
+from typing import List, Union
+
+def comb(hour, minute, day: datetime=datetime.now()):
+    return datetime.combine(day, dt.time(hour=hour, minute=minute))
 
 def f_time(num:Union[int | str]):
     return num if int(num) > 9 else f'0{num}'
@@ -14,237 +16,60 @@ def round_minutes_to_nearest_value(d_time, value=15):
     rounded_dt = d_time.replace(minute=minute, second=0, microsecond=0)
     return rounded_dt
 
-
 def get_available_pet_walk_time(
-        selected_day: dt.datetime,
-        walk_times: List[Tuple[dt.datetime, dt.datetime]],
-        duration_min:int,
-        work_start_hour:int=8,
-        work_end_hour:int=18):
+        walk_times:List[datetime],
+        selected_day=datetime.now(),
+        duration=15,
+        start_hour=8,
+        end_hour=18,
+):
+    step_minutes = 15
+    duration_in_steps = duration / step_minutes
+    walk_times = [wt for wt in walk_times if wt[0].month == selected_day.month and wt[0].day == selected_day.day]
+    start = datetime.combine(selected_day, dt.time(hour=start_hour))
+    end = datetime.combine(selected_day, dt.time(hour=end_hour))
 
-    work_end = dt.datetime.now().replace(hour=work_end_hour, minute=0, second=0, microsecond=0)
-    available_times = []
+    walk_times_starts = [w[0] for w in walk_times]
 
-    current_time = round_minutes_to_nearest_value(
-        selected_day.replace(hour=selected_day.hour if work_start_hour <= selected_day.hour else work_start_hour)
-    )
+    day_slots = []
 
-    while current_time + dt.timedelta(minutes=duration_min) <= work_end:
+    cursor = start.replace()
+    while cursor < end:
+        day_slots.append(cursor)
+        cursor += timedelta(minutes=step_minutes)
+    day_slots.append(end)
 
-        is_available = True
-        for walk_time in walk_times:
+    for walk_start, walk_end in walk_times:
+        cursor = walk_start.replace()
+        if cursor.month == start.month and cursor.day == start.day:
+            while cursor < walk_end:
+                day_slots.pop(day_slots.index(cursor))
+                cursor += timedelta(minutes=step_minutes)
 
-            if walk_time[0] <= current_time < walk_time[1]:
-                is_available = False
+    available_slots = []
+
+    line = day_slots[:]
+    start_cursor = line[0]
+    while start_cursor < end:
+        steps = 0
+        start_cursor = line[0]
+
+        for n, slot in enumerate(line):
+            try:
+                next_slot = slot + timedelta(minutes=step_minutes)
+                if next_slot == line[n+1] or next_slot in walk_times_starts:
+                    if not start_cursor in available_slots:
+                        steps += 1
+                        if steps == duration_in_steps:
+                            available_slots.append(start_cursor)
+                            line = line[1:]
+                            break
+                    if next_slot in walk_times_starts:
+                        line = line[1:]
+                        break
+            except IndexError:
+                start_cursor = end
                 break
 
-        if is_available:
-            available_times.append(f'{f_time(current_time.hour)}:{f_time(current_time.minute)}')
+    return available_slots
 
-        current_time += dt.timedelta(minutes=duration_min)
-
-    return available_times
-
-def get_available_pet_walk_time2(
-        booked_time_frames,
-        min_time_slot_in_minutes: int = 15,
-        selected_day: None | dt.datetime = None,
-        work_start_in_hour=8,
-        work_end_in_hour=18,
-):
-    if not selected_day:
-        selected_day = dt.datetime.now()
-
-    min_time_slot = dt.timedelta(minutes=min_time_slot_in_minutes)
-    open_time = dt.datetime.combine(selected_day, dt.time(hour=work_start_in_hour))
-    close_time = dt.datetime.combine(selected_day, dt.time(hour=work_end_in_hour))
-
-    booked_time_slots = []
-    all_time_slots = []
-
-    while open_time <= close_time - min_time_slot:
-        all_time_slots.append(open_time.strftime('%H:%M'))
-        open_time += min_time_slot
-
-    for start_time, end_time in booked_time_frames:
-        while start_time < end_time:
-            booked_time_slots.append(start_time.strftime('%H:%M'))
-            start_time += min_time_slot
-
-    available_times = sorted(set(all_time_slots) - set(booked_time_slots))
-    return available_times
-
-
-booked_time_frames = [
-    (dt.datetime(2023, 9, 21, 8, 0), dt.datetime(2023, 9, 21, 8, 15)),
-    (dt.datetime(2023, 9, 21, 8, 15), dt.datetime(2023, 9, 21, 8, 30)),
-    (dt.datetime(2023, 9, 21, 9, 0), dt.datetime(2023, 9, 21, 9, 45)),
-    (dt.datetime(2023, 9, 21, 10, 0), dt.datetime(2023, 9, 21, 10, 30)),
-]
-
-available_times2 = get_available_pet_walk_time2(
-     booked_time_frames=booked_time_frames,
-     min_time_slot_in_minutes=15,
-     selected_day=dt.datetime(2023, 9, 28),
-     work_start_in_hour=8,
-     work_end_in_hour=18,
-)
-
-
-class TestAvailablePetWalkTime(unittest.TestCase):
-
-    def test_1_booked_time_frames_small(self):
-        booked_time_frames = []
-
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=15,
-            selected_day=dt.datetime(2023, 9, 28),
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['08:00', '08:15', '08:30', '08:45', '09:00','09:15', '09:30',
-                     '09:45', '10:00', '10:15', '10:30', '10:45','11:00', '11:15',
-                     '11:30', '11:45', '12:00', '12:15', '12:30','12:45', '13:00',
-                     '13:15', '13:30', '13:45', '14:00', '14:15','14:30', '14:45',
-                     '15:00', '15:15', '15:30', '15:45', '16:00','16:15', '16:30',
-                     '16:45', '17:00', '17:15', '17:30', '17:45']
-        self.assertEqual(available_pet_walk_time, container)
-
-    def test_2_booked_time_frames_medium(self):
-        booked_time_frames = [
-            (dt.datetime(2023, 9, 28, 8, 0), dt.datetime(2023, 9, 28, 8, 15)),
-            (dt.datetime(2023, 9, 28, 8, 15), dt.datetime(2023, 9, 28, 8, 30)),
-            (dt.datetime(2023, 9, 28, 9, 0), dt.datetime(2023, 9, 28, 9, 45)),
-            (dt.datetime(2023, 9, 28, 10, 0), dt.datetime(2023, 9, 28, 10, 30)),
-        ]
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=15,
-            selected_day=dt.datetime(2023, 9, 28),
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['08:30', '08:45', '09:45', '10:30', '10:45', '11:00', '11:15',
-                     '11:30', '11:45', '12:00', '12:15', '12:30', '12:45', '13:00',
-                     '13:15', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45',
-                     '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30',
-                     '16:45', '17:00', '17:15', '17:30', '17:45']
-        self.assertEqual(available_pet_walk_time, container)
-
-    def test_3_booked_time_frames_medium_mixed(self):
-        booked_time_frames = [
-            (dt.datetime(2023, 9, 28, 9, 0), dt.datetime(2023, 9, 28, 9, 45)),
-            (dt.datetime(2023, 9, 28, 10, 0), dt.datetime(2023, 9, 28, 10, 30)),
-            (dt.datetime(2023, 9, 28, 8, 0), dt.datetime(2023, 9, 28, 8, 15)),
-            (dt.datetime(2023, 9, 28, 8, 15), dt.datetime(2023, 9, 28, 8, 30)),
-        ]
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=15,
-            selected_day=dt.datetime(2023, 9, 28),
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['08:30', '08:45', '09:45', '10:30', '10:45', '11:00', '11:15',
-                     '11:30', '11:45', '12:00', '12:15', '12:30', '12:45', '13:00',
-                     '13:15', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45',
-                     '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30',
-                     '16:45', '17:00', '17:15', '17:30', '17:45']
-        self.assertEqual(available_pet_walk_time, container)
-
-    def test_4_booked_time_frames_large_and_last_slot(self):
-        booked_time_frames = [
-            (dt.datetime(2023, 9, 28, 8, 0), dt.datetime(2023, 9, 28, 8, 15)),
-            (dt.datetime(2023, 9, 28, 8, 15), dt.datetime(2023, 9, 28, 8, 30)),
-            (dt.datetime(2023, 9, 28, 8, 30), dt.datetime(2023, 9, 28, 8, 45)),
-            (dt.datetime(2023, 9, 28, 8, 45), dt.datetime(2023, 9, 28, 9, 0)),
-            (dt.datetime(2023, 9, 28, 9, 0), dt.datetime(2023, 9, 28, 9, 15)),
-            (dt.datetime(2023, 9, 28, 9, 15), dt.datetime(2023, 9, 28, 9, 30)),
-            (dt.datetime(2023, 9, 28, 9, 30), dt.datetime(2023, 9, 28, 9, 45)),
-            (dt.datetime(2023, 9, 28, 9, 45), dt.datetime(2023, 9, 28, 10, 0)),
-            (dt.datetime(2023, 9, 28, 10, 0), dt.datetime(2023, 9, 28, 10, 15)),
-            (dt.datetime(2023, 9, 28, 10, 15), dt.datetime(2023, 9, 28, 10, 30)),
-            (dt.datetime(2023, 9, 28, 10, 30), dt.datetime(2023, 9, 28, 10, 45)),
-            (dt.datetime(2023, 9, 28, 10, 45), dt.datetime(2023, 9, 28, 11, 0)),
-            (dt.datetime(2023, 9, 28, 11, 0), dt.datetime(2023, 9, 28, 11, 15)),
-            (dt.datetime(2023, 9, 28, 11, 15), dt.datetime(2023, 9, 28, 11, 30)),
-            (dt.datetime(2023, 9, 28, 11, 30), dt.datetime(2023, 9, 28, 11, 45)),
-            (dt.datetime(2023, 9, 28, 11, 45), dt.datetime(2023, 9, 28, 12, 0)),
-            (dt.datetime(2023, 9, 28, 12, 0), dt.datetime(2023, 9, 28, 12, 15)),
-            (dt.datetime(2023, 9, 28, 12, 15), dt.datetime(2023, 9, 28, 12, 30)),
-            (dt.datetime(2023, 9, 28, 12, 30), dt.datetime(2023, 9, 28, 12, 45)),
-            (dt.datetime(2023, 9, 28, 12, 45), dt.datetime(2023, 9, 28, 13, 0)),
-            (dt.datetime(2023, 9, 28, 13, 0), dt.datetime(2023, 9, 28, 13, 15)),
-            (dt.datetime(2023, 9, 28, 13, 15), dt.datetime(2023, 9, 28, 13, 30)),
-            (dt.datetime(2023, 9, 28, 13, 30), dt.datetime(2023, 9, 28, 13, 45)),
-            (dt.datetime(2023, 9, 28, 13, 45), dt.datetime(2023, 9, 28, 14, 0)),
-            (dt.datetime(2023, 9, 28, 14, 0), dt.datetime(2023, 9, 28, 14, 15)),
-            (dt.datetime(2023, 9, 28, 14, 15), dt.datetime(2023, 9, 28, 14, 30)),
-            (dt.datetime(2023, 9, 28, 14, 30), dt.datetime(2023, 9, 28, 14, 45)),
-            (dt.datetime(2023, 9, 28, 14, 45), dt.datetime(2023, 9, 28, 15, 0)),
-            (dt.datetime(2023, 9, 28, 15, 0), dt.datetime(2023, 9, 28, 15, 15)),
-            (dt.datetime(2023, 9, 28, 15, 15), dt.datetime(2023, 9, 28, 15, 30)),
-            (dt.datetime(2023, 9, 28, 15, 30), dt.datetime(2023, 9, 28, 15, 45)),
-            (dt.datetime(2023, 9, 28, 15, 45), dt.datetime(2023, 9, 28, 16, 0)),
-            (dt.datetime(2023, 9, 28, 16, 0), dt.datetime(2023, 9, 28, 16, 15)),
-            (dt.datetime(2023, 9, 28, 16, 15), dt.datetime(2023, 9, 28, 16, 30)),
-            (dt.datetime(2023, 9, 28, 16, 30), dt.datetime(2023, 9, 28, 16, 45)),
-            (dt.datetime(2023, 9, 28, 16, 45), dt.datetime(2023, 9, 28, 17, 0)),
-            (dt.datetime(2023, 9, 28, 17, 0), dt.datetime(2023, 9, 28, 17, 15)),
-            (dt.datetime(2023, 9, 28, 17, 15), dt.datetime(2023, 9, 28, 17, 30)),
-            (dt.datetime(2023, 9, 28, 17, 30), dt.datetime(2023, 9, 28, 17, 45)),
-        ]
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=15,
-            selected_day=dt.datetime(2023, 9, 28),
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['17:45']
-        self.assertEqual(available_pet_walk_time, container)
-
-    def test_5_min_time_slot_in_minutes_30(self):
-        booked_time_frames = []
-
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=30,
-            selected_day=dt.datetime(2023, 9, 28),
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['08:00', '08:30', '09:00', '09:30', '10:00',
-                     '10:30', '11:00', '11:30', '12:00', '12:30',
-                     '13:00', '13:30', '14:00', '14:30', '15:00',
-                     '15:30', '16:00', '16:30', '17:00', '17:30']
-
-        self.assertEqual(available_pet_walk_time, container)
-
-    def test_6_min_time_slot_in_minutes_60(self):
-        booked_time_frames = []
-
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=60,
-            selected_day=dt.datetime(2023, 9, 28),
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
-
-        self.assertEqual(available_pet_walk_time, container)
-
-    def test_7_not_selected_day(self):
-        booked_time_frames = []
-
-        available_pet_walk_time = get_available_pet_walk_time2(
-            booked_time_frames=booked_time_frames,
-            min_time_slot_in_minutes=60,
-            work_start_in_hour=8,
-            work_end_in_hour=18,
-        )
-        container = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
-
-        self.assertEqual(available_pet_walk_time, container)
