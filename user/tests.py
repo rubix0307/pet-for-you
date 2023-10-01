bef6e7d901edea2b038f68ab9281556a0895ce36
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-10-01 14:43:08 +0200

    update migrations & tests

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestAuth(TestCase):
+
+    def test_register(self):
+        c = Client()
+
+        data = {
+            'first_name':'first_name',
+            'last_name':'last_name',
+            'email':'test_email@example.com',
+            'password1':'password',
+            'password2':'password',
+            'registration_form':'', # to identify form processing
+        }
+        response = c.post(path=reverse('user_auth'), data=data)
+
+        print(response.status_code)  # Просто для проверки статуса ответа
